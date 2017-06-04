2bf1a177c5d62da4a9a671f149d5e81b09a4ac21
Rostyslav Stepanyak <rostyslav.stepanyak@darkmatter.ae>
2017-06-04 15:24:16 +0400

    move chat framework to root repo folder

---
+++ MTChatTestProject/MTChatTest/build/MTChatTest.build/Release-iphonesimulator/MTChatTest.build/Objects-normal/i386/main.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/Rostyslav.Stepanyak/Work/iOS/MTChat/MTChatTestProject/MTChatTest/MTChatTest/main.m \
+  /Users/Rostyslav.Stepanyak/Work/iOS/MTChat/MTChatTestProject/MTChatTest/MTChatTest/AppDelegate.h
